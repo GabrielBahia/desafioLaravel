90a67fdf62ac8bbfe782d7b3372283c08896872d
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-08-18 23:56:48 -0300

    Estilizando form de criar produto

--- resources/views/components/products/form.blade.php
+++ resources/views/components/products/form.blade.php
@@ -1,31 +1,33 @@
-<form action="{{ $action }}" method="post">
-    @csrf
+<div class="bloco-secundario">
+    <form class="form-edit-style" action="{{ $action }}" method="post">
+        @csrf
 
-    @isset($product)
-        @method('PUT')
-    @endisset
+        @isset($product)
+            @method('PUT')
+        @endisset
 
-    <div class="mt-5 mb-3 ">
-        <label for="nome" class="form-label">Nome:</label>
-        <input type="text" id="nome" name="nome" class="form-control" 
-        @isset($product->nome)value="{{ $product->nome }}" @endisset>
+        <div>
+            <label for="nome" class="form-label label-form-style"> Nome: </label>
+            <input type="text" id="nome" name="nome" class="form-control" 
+            @isset($product->nome)value="{{ $product->nome }}" @endisset>
 
-        <label for="sabor" class="form-label">Sabor:</label>
-        <input type="text" id="sabor" name="sabor" class="form-control"
-        @isset($product->sabor)value="{{ $product->sabor }}" @endisset>
+            <label for="sabor" class="form-label label-form-style">Sabor:</label>
+            <input type="text" id="sabor" name="sabor" class="form-control"
+            @isset($product->sabor)value="{{ $product->sabor }}" @endisset>
 
-        <label for="preco" class="form-label">Preço:</label>
-        <input type="text" id="preco" name="preco" class="form-control"
-        @isset($product->preco)value="{{ $product->preco }}" @endisset>
+            <label for="preco" class="form-label label-form-style">Preço:</label>
+            <input type="text" id="preco" name="preco" class="form-control"
+            @isset($product->preco)value="{{ $product->preco }}" @endisset>
 
-        <label for="descricao">Descrição:</label>
-        <textarea class="form-control" id="descricao" name="descricao" rows="3">  
-            @isset($product->descricao){{$product->descricao}}@endisset
-        </textarea>
+            <label class="form-label label-form-style" for="descricao">Descrição:</label>
+            <textarea class="form-control" id="descricao" name="descricao" rows="3">  
+                @isset($product->descricao){{$product->descricao}}@endisset
+            </textarea>
 
-        <label for="foto" class="form-label">Foto:</label>
-        <input type="text" id="foto" name="foto" class="form-control"
-        @isset($product->foto)value="{{ $product->foto }}" @endisset>
-    </div>
-    <button type="submit" class="btn btn-lg btn-primary px-4">Criar</button>
-</form>
+            <label for="foto" class="form-label label-form-style">Foto:</label>
+            <input type="text" id="foto" name="foto" class="form-control"
+            @isset($product->foto)value="{{ $product->foto }}" @endisset>
+        </div>
+        <button type="submit" class="btn btn-lg botao1-2 px-4 mt-3">Criar</button>
+    </form>
+</div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,7 +8,7 @@
         @endisset    
 
         <div class="botao1">  
-            <a href="{{ route('products.create') }}" class="btn btn-dark mb-2 me-md-2">Criar novo produto</a>
+            <a href="{{ route('products.create') }}" class="botao1-2 btn mb-2 me-md-2">Criar novo produto</a>
         </div>
 
         <div class="bloco-secundario">
@@ -28,8 +28,8 @@
                             <td>{{ $product->nome }}</td>
                             <td>R$ {{ $product->preco }}</td>
                             <td class="tds-style">
-                                <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary tds-item-style">Ver</a>
-                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary tds-item-style">Editar</a>
+                                <a href="{{ route('products.show', $product->id) }}" class="botoes-marrom btn tds-item-style">Ver</a>
+                                <a href="{{ route('products.edit', $product->id) }}" class="botoes-marrom btn  tds-item-style">Editar</a>
                                 <form action="{{ route('products.destroy', $product->id) }}" method="post">
                                     @csrf
                                     @method('DELETE')
