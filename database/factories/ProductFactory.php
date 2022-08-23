167d0870f8dc58ff3d2376d3ade14659b8adb346
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-08-23 16:16:50 -0300

    Criando seeds e factory de product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nome' => fake()->unique()->word,
+            'sabor' => fake()->colorName,
+            'preco' => fake()->randomFloat(2, 1, 10),
+            'descricao' => fake()->name(),
+            'foto' => fake()->name(),
+        ];
+    }
+}
